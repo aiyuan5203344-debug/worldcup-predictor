5caa64d6bf6cf3464b6b31b9205623d151511fa5
yuan-20022 <2772762002@qq.com>
2026-06-14 23:49:32 +0800

    fix: simplify CORS to origin:true

--- server/src/index.js
+++ server/src/index.js
@@ -94,10 +94,7 @@ app.use(requestLogger)
 
 // CORS configuration
 app.use(cors({
-  origin: (origin, callback) => {
-    // Allow all origins - mobile apps, curl, same-origin with crossorigin attribute
-    callback(null, true)
-  },
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
