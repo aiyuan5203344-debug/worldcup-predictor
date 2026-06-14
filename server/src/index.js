9199bbb0b2720d3fbafc146721c35b89a8563dcf
yuan-20022 <2772762002@qq.com>
2026-06-14 23:34:49 +0800

    fix: add unsafe-inline to script-src CSP to unblock loading screen hiding

--- server/src/index.js
+++ server/src/index.js
@@ -71,7 +71,7 @@ app.use(helmet({
   contentSecurityPolicy: config.isProduction ? {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "data:", "https:"],
